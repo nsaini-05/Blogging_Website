2f87020ccd27cbba981493bf545fd76a09e3f627
Neeraj <asaini7788@gmail.com>
2020-12-15 18:34:38 -0500

    Add Databse

--- server.js
+++ server.js
@@ -4,11 +4,18 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 var _= require('lodash');
-const posts = [];
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+
+const mongoose = require("mongoose");
+mongoose.connect('mongodb+srv://admin-neeraj:Test123@cluster0.y7iw3.mongodb.net/postDB', {useNewUrlParser : true , useUnifiedTopology : true});
+const postSchema = {postTitle : String , postBody : String};
+const Post = new mongoose.model("post" , postSchema);
+
+
+
+const homeStartingContent = "Post Related to Home";
+const aboutContent = "About Page";
+const contactContent = "Contact Page";
 
 const app = express();
 
@@ -18,10 +25,27 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 
-app.get("/",function(req , res)
-{
-  res.render('home',{startingContent : homeStartingContent , posts : posts});
+app.get("/",function(req , res){
+
+  Post.find({},function(err,posts){
+    if(posts.length === 0 )
+    {
+        res.render("home", {startingContent : homeStartingContent , posts : [] });
+    }
+
+    else {
+      res.render("home", {startingContent : homeStartingContent , posts : posts});
+    }
+
+
+
+  });
+
 });
+
+
+
+
 
 app.get("/about" , function(req , res)
 {
@@ -45,35 +69,37 @@ app.get("/compose" , function(req , res)
 
 app.post("/compose" , function(req,res)
 {
-  var post ={
-    title : req.body.postTitle,
-    body :  req.body.postBody
-  };
+  var post = new Post({
+    postTitle : req.body.postTitle,
+    postBody : req.body.postBody
+  });
 
-  posts.push(post);
-  res.redirect("/");
+  post.save(function(err){
+
+    if (!err){
+
+      res.redirect("/");
+
+    }
+
+  });
 
 });
 
 
 
-app.get('/posts/:postName', function (req, res) {
+app.get('/posts/:postId', function (req, res) {
 
-posts.forEach(function(post){
-
-  const existingTitle = _.lowerCase(post.title);
-  const requiredTitle = _.lowerCase(req.params.postName)
-
-  if(existingTitle === requiredTitle)
+  const requiredId= req.params.postId;
+  Post.findOne({_id : requiredId} , function(err , post)
+{
+  if(!err)
   {
-    res.render('post',{postTitle :  post.title , postContent : post.body});
+    res.render('post' , {postTitle : post.postTitle , postContent :  post.postBody});
   }
-})
+});
 
-
-res.redirect("/");
-
-})
+})  ;
 
 
 
